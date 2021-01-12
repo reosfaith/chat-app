a4b058fed38f18ae88035bb396a72e5f103a16f8
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-01-12 10:26:34 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :controllers
   get 'messages/index'
   root to: "messages#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_092140) do
+ActiveRecord::Schema.define(version: 2021_01_11_104852) do
 
   create_table "controllers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,6 +23,19 @@ ActiveRecord::Schema.define(version: 2021_01_11_092140) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_controllers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
