c1889abb3a1824b3fbd5e0a30be9f026009f241e
reosfaith <76994477+reosfaith@users.noreply.github.com>
2021-01-12 14:18:52 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_104852) do
+ActiveRecord::Schema.define(version: 2021_01_12_051622) do
 
   create_table "controllers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_01_11_104852) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_controllers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
